ea60a0a13e5e793ba13e1575f20f56204233b7b3
Vijay Kethanaboyina <vijaykethanaboyina@gmail.com>
2024-06-16 21:04:04 -0700

    added some dummy files

---
+++ main.py
@@ -0,0 +1,16 @@
+def add_two_numbers(a, b):
+    ''' Takes in two nums a and b and returns their sum '''
+    return a + b
+
+def multiply_two_numbers(a, b):
+    ''' Takes in two nums a and b and returns their product '''
+    return a * b
+
+def subtract_two_numbers(a, b):
+    ''' Takes in two nums a and b and returns their difference '''
+    return a - b
+
+if __name__ == '__main__':
+    print(add_two_numbers(3, 4))
+    print(multiply_two_numbers(3, 4))
+    print(subtract_two_numbers(3, 4))
